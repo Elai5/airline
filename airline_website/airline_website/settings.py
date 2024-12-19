f5782a41dc3d5b3e5feb3ba29f62a942f2903815
Elai5 <yvettelaine007@gmail.com>
2024-12-19 11:42:50 +0300

    trial2

--- airline_website/airline_website/settings.py
+++ airline_website/airline_website/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'contact',
     'service',
     'accounts',
+    'home',
     
 ]
 
--- airline_website/settings.py
+++ airline_website/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'contact',
     'accounts',
     'service',
+    'home',
     
 ]
 

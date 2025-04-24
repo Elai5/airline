101ac308ea870716907eafcce4ff0f2f08e6c16e
Elai5 <yvettelaine007@gmail.com>
2025-04-24 16:00:39 +0300

    booking added,picture dimensions changed

--- airline_website/airline_website/settings.py
+++ airline_website/airline_website/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'airline_website.urls'
